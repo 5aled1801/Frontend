4e7ae098ff3347ecb7a37e792f2f4e74ce81799f
Khaled Hassan <99255706+5aled1801@users.noreply.github.com>
2023-11-07 13:58:22 +0300

    Delete vue/E-commerce directory

--- vue/E-commerce/src/assets/rigester.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var signorlogin = document.getElementById("signorlogin")
-    const userfound=localStorage.getItem('name')
-    
-    if (userfound) {
-        signorlogin.style.display = "none"
-    }
-})
